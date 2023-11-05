d2d35d9d8264d090074db8e5022f9f6fdbb841c8
Prashant Bhandari <santhprashant@gmail.com>
2023-11-05 22:44:36 +0545

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,18 @@
-print("hello world")
+# ask use for their name
+"""
+name = input("what's your name?").strip().title()
+first, last = name.split(" ")
+print(f"hello, {first} ")
+"""
+
+
+def main():
+    name = input("What is your name? ")
+    hello(name)
+
+
+
+def hello(to="world"):
+    print("Hello,",to)
+
+main()
