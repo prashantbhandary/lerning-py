874eebe94fed64e1f1adcebe3f0209621df89038
Prashant Bhandari <santhprashant@gmail.com>
2023-11-07 11:44:23 +0545

    Conditional
    learned condition

--- conditional/compare.py
+++ conditional/compare.py
@@ -1,7 +1,7 @@
 x = int (input("what is the x?"))
 y = int (input("what is the y?"))
 
-if x>y or  x<y::
+if x>y or  x<y:
     print("x not equal y") 
-else
+else:
     print("equal")
---
+++ conditional/house.py
@@ -0,0 +1,7 @@
+name = input ("what's your name")
+
+match name:
+    case "Harry" | "Hemaioni" | "Ron":
+         print("Gryffindor")
+    case _:
+        print("who??")
